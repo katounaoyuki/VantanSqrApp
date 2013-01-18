6ae1f2dc17ddada152ef5ba1e405806d731c52ab
yagitoshiro <yagitoshiro@gmail.com>
2013-01-18 11:49:38 +0900

    today's happenings

--- Resources/ui/handheld/Place.js
+++ Resources/ui/handheld/Place.js
@@ -1,5 +1,50 @@
 function Place(){
-  var self = Ti.UI.createWindow();
+  var self = Ti.UI.createWindow({
+    backgroundColor: 'White'
+  });
+
+  var wrap = Ti.UI.createView({
+    layout: 'vertical'
+  });
+
+  Ti.Geolocation.purpose = "あなたの居場所をみんなに言いふらして追い込むため";
+  Ti.Geolocation.getCurrentPosition(function(e){
+    var latitude, longitude;
+    if(e.error){
+      //TODO ごめん
+      var image = Ti.UI.createImageView({
+        top: 0,
+        image:'/images/error_cat.jpg',
+        width: Ti.UI.FILL
+      });
+      wrap.add(image);
+      wrap.add(Ti.UI.createLabel({
+        text:'現在位置を取得できると思ったか？',
+        top:20
+      }));
+    }else{
+      latitude = e.coords.latitude;
+      longitude = e.coords.longitude;
+      var map = Ti.Map.createView({
+        top: 0,
+        width: Ti.UI.FILL,
+        height: Ti.Platform.displayCaps.platformHeight / 3,
+        mapType: Titanium.Map.STANDARD_TYPE,
+        animate:true,
+        region: {
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+        },
+        regionFit:true,
+        userLocation:true
+      });
+      wrap.add(map);
+    }
+  });
+
+  self.add(wrap);
   return self;
 }
 
---
+++ tmp.js
@@ -0,0 +1,5 @@
+function CreateMap(e){
+
+}
+
+module.exports = CreateMap;
